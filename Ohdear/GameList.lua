a743667baff77884beebe6cf4db115612ec5bea5
ohdear-erine <yoonacyc@gmail.com>
2026-04-23 16:42:57 +0800

    Create GameList.lua

---
+++ Ohdear/GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [4490140733] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/main.lua", -- My Restaurant
+  [13200523806] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/plaza.lua" -- Trading Plaza
+}
+
+return Games
