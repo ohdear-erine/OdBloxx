d8d66410140b386d65bacfe78849ad23ec6dd10f
ohdear-erine <yoonacyc@gmail.com>
2026-04-25 11:59:32 +0800

    Create GameList.lua

---
+++ Resto/GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [112490729816320] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/soccer.lua",
+  [124069176440953] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/soccerplaza.lua"
+}
+
+return Games
