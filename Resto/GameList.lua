aaac4873a1a9c9bbee6e28b9daa1d131a0dc6454
ohdear-erine <yoonacyc@gmail.com>
2026-05-23 07:55:18 +0800

    Update GameList.lua

--- Resto/GameList.lua
+++ Resto/GameList.lua
@@ -1,6 +1,7 @@
 local Games = {
   [112490729816320] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/soccer.lua",
-  [124069176440953] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/soccerplaza.lua"
+  [124069176440953] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/soccerplaza.lua",
+  [77843161404023] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/RunResto.lua",
 }
 
 return Games
