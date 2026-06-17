158bcd9686e1c28435a0c7ba65a8b78fd64d91ce
ohdear-erine <yoonacyc@gmail.com>
2026-06-18 07:57:44 +0800

    Update GameList.lua

--- Resto/GameList.lua
+++ Resto/GameList.lua
@@ -4,8 +4,14 @@ local Games = {
   [77843161404023] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/RunResto.lua",
   [107646426076756] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/RingFarm.lua",
   [101736964164901] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/warnet.lua",
-  [97598239454123] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/gag2.lua",
-  [77085202503540] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/gag2.lua",
+  [97598239454123] = {
+    [1] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/gags2.lua",
+    [2] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/rbgag2.lua"
+  },
+  [77085202503540] = {
+    [1] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/gags2.lua",
+    [2] = "https://raw.githubusercontent.com/ohdear-erine/OdBloxx/refs/heads/main/Resto/rbgag2.lua"
+  }
 }
 
 return Games
